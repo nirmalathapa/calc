203db4817ef57bd837a39078f19f1da3d890f159
nirmala <nirmala@surf.net.np>
2018-04-27 17:06:23 +0200

    change to es6 syntax

--- main.js
+++ main.js
@@ -1,47 +1,53 @@
-var keys  = [].slice.call(document.getElementsByClassName('item'));
-var operators = ['/', '*', '-', '+', '÷', '×', '.'];
+//display the output from the click
+const display = document.querySelector('.display');
 
-keys.forEach(function (el){
-    el.addEventListener('click', myfunction, false);
-});
+//get the keys from html collection
+const keys = document.getElementsByClassName('item'); 
 
-function myfunction(e){
-    
-    var display = document.querySelector('.display'); // initializing
-    var previousVal = display.innerHTML;    //assignment 
-    
-    var currentVal = this.innerHTML;
-    
-    var op = document.getElementsByClassName('op');
-    if(operators.includes(currentVal) && previousVal === ""){
-        return;
-    }
-    
-    if(operators.includes(currentVal) && operators.includes(previousVal.slice(-1))){
-        
-        return;
-    }
+//store operators sign
+const op = ['/', '+', '-', '%', '*', '÷', '×', '+/-', '.'];
 
-    if(currentVal === "AC"){
-        display.textContent = "";
-    }
-    else if(currentVal === "+/-"){
-        display.textContent = "-";    
-     }
-    else if(currentVal === "%"){
-        var percent = previousVal/100;
-        display.textContent = percent;
-    }
-    else if(currentVal === "="){
-        var calculate = previousVal;        
-        var lastNum = calculate[calculate.length -1];
-        calculate = calculate.replace(/×/g, '*').replace(/÷/g, '/');
-        if(calculate){            
-            display.textContent = eval(calculate);
+// function that clears the disply
+function allClear(){
+    display.textContent = "";
+}
+ 
+//add click event on each keys;
+for(let i = 0; i < keys.length; i++){
+    
+    keys[i].addEventListener('click', function(e){
+        let currentVal = e.target.textContent;
+        let previousVal = display.textContent;
+        console.log('pre',previousVal);
+        console.log('cur',currentVal);
+        let lastInd = previousVal.slice(-1);
+        console.log('iam', lastInd);
+        //disable when the operator sign is clicked
+        if(op.indexOf(currentVal) > -1 && previousVal == ""){
+            return;
         }
-    }
-    else{
-        display.textContent= previousVal + currentVal;
-    }
-};
+        //disable more than one click of an operator sign 
+        if(op.indexOf(lastInd) !== -1 && op.indexOf(currentVal) !== -1){
+            return;
+        };
 
+        if(currentVal === "AC"){
+            allClear();
+        } else if(currentVal === "="){
+            let calculate = previousVal;
+            //replace × and ÷
+            let replaceOp = calculate.replace('×', '*').replace('÷', '/');
+            display.textContent = eval(replaceOp);
+            //should not be able to click operators more than once
+        }else if(currentVal === "%"){
+            let percent = previousVal / 100; //calculate percent when clicked on %
+            display.textContent = percent;
+
+        }else if(currentVal === "+/-"){
+            display.textContent = '-';   
+        }
+        else{
+            display.textContent = previousVal + currentVal;
+        }
+    })
+}
